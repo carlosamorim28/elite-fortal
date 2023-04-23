12c5d5e0dc8b8694030a67c915ad55e17b49fd2c
Carlos Jorge <carlos.amorim@loldesigin.com.br>
2023-04-23 11:37:33 -0300

    Teste com vontade

--- src/pages/api/test.ts
+++ src/pages/api/test.ts
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import prismaCliente from "@/prisma";
-import { Users } from "@prisma/client";
+import { PrismaClient, Users } from "@prisma/client";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
@@ -11,18 +11,10 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<any>
 ) {
-  fetch("https://www.dnd5eapi.co/api/ability-scores/cha", {
-    method: "GET",
-  })
-    .then((enconderElements) => {
-      enconderElements.json().then((elements) => {
-        res.status(200).send(elements);
-      });
-    })
-    .catch((error) => {
-      res.status(500).send({ name: "Error: ", error });
-    })
-    .catch((error) => {
-      res.status(500).send({ name: "Error: ", error });
+  const prismaCliente = new PrismaClient();
+  prismaCliente.$connect().then(() => {
+    prismaCliente.users.findMany().then((elements) => {
+      res.status(200).send(elements);
     });
+  });
 }
