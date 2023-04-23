d7db4f3dfa5bc5a4736e9764c8fb5b04a29805d0
Carlos Jorge <carlos.amorim@loldesigin.com.br>
2023-04-23 08:36:05 -0300

    vaidevez

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -29,7 +29,7 @@ export default function Login(): React.ReactElement {
       router.push('/home')
 
     } catch(error: unknown){
-      throw new Error({ error })
+      throw new Error("Erro ",{ error })
       console.log("Deu ruim em")
 
     }
--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -30,7 +30,7 @@ export default function Register(): React.ReactElement{
     
       alert("Não foi possível criar a conta, verifique se o email já está sendo usado")
     
-      throw new Error({error})
+      throw new Error("Erro",{error})
 
     }
   }
